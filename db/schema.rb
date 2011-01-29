22742b9c50db5627a9d431c1dd82245d59f9f877
Chinmay Kulkarni <chinmay007@gmail.com>
2011-01-29 15:42:36 -0800

    Added admin role migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110129230255) do
+ActiveRecord::Schema.define(:version => 20110129233904) do
 
   create_table "adapted_pages", :force => true do |t|
     t.string   "url"
@@ -19,9 +19,9 @@ ActiveRecord::Schema.define(:version => 20110129230255) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "email",                               :default => "",    :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "",    :null => false
+    t.string   "password_salt",                       :default => "",    :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20110129230255) do
     t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                               :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
