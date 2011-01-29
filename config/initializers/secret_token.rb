3ac9fb40f9542822ef7aef9eb73a5d91cb97105b
Chinmay Kulkarni <chinmay007@gmail.com>
2011-01-29 14:23:26 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helsinki::Application.config.secret_token = 'ca6b386699ed0372d09600c48e8abbe9c0ac1574fee4fb8bcab4179ef70f31ebacbf35c7b3a0d509d090cdf869fbd2536439f590df8c532af083ebedfa6af6ed'
